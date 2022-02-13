555c3dba895355b88051b6ea1d09cd819aec51f6
Manoj Bhardwaj <manoj_4@yahoo.com>
2022-02-12 17:23:55 -0700

    Modifying unit test for actions test

--- test/unit_tests/inaugural-test.cpp
+++ test/unit_tests/inaugural-test.cpp
@@ -1,9 +1,16 @@
 #include <gtest/gtest.h>
+#include <string>
 
 TEST(First, Try)
 {
    int a = 2;
    int b = 2*a;
    EXPECT_EQ(2*a, b);
+}
+
+TEST(Second, Try)
+{
+   const std::string message = "Hello World";
+   EXPECT_EQ(std::string("Hello World"), message);
 }
 
