76d679a1193ed7404d547d03d7721e6ecf8821c1
Manoj Bhardwaj <manoj_4@yahoo.com>
2022-02-12 18:53:38 -0700

    Intentional failure

--- test/unit_tests/inaugural-test.cpp
+++ test/unit_tests/inaugural-test.cpp
@@ -3,20 +3,26 @@
 
 TEST(First, Try)
 {
-   int a = 2;
-   int b = 2*a;
-   EXPECT_EQ(2*a, b);
+    int a = 2;
+    int b = 2*a;
+    EXPECT_EQ(2*a, b);
 }
 
 TEST(Second, Try)
 {
-   const std::string message = "Hello World";
-   EXPECT_EQ(std::string("Hello World"), message);
+    const std::string message = "Hello World";
+    EXPECT_EQ(std::string("Hello World"), message);
 }
 
 TEST(Third, Try)
 {
-   double estimate = 1.0003;
-   EXPECT_NEAR(1.00029999, estimate, 1.0e-6);
+    double estimate = 1.0003;
+    EXPECT_NEAR(1.00029999, estimate, 1.0e-6);
+}
+
+TEST(Fourth, Try)
+{
+    int number = 2;
+    EXPECT_EQ(3, number); 
 }
 
