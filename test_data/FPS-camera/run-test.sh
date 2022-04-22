ef4bf6dd4c8f384965de156e6058b3b0a75395ea
Manoj Bhardwaj <manoj_4@yahoo.com>
2022-04-22 17:52:43 -0600

    Trying to see if test will run in github actions.

--- test_data/FPS-camera/run-test.sh
+++ test_data/FPS-camera/run-test.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-./FPS-camera -p test.log
+../../test/SRE-Test-FPS-camera -p test.ui_events
 
-if (../bin/imgcmp -v -o diff1.png -t 0.0 -e 0.0% Test0_base.png Test0.png == -1) {
-    fail = true
-}
+#if (../bin/imgcmp -v -o diff1.png -t 0.0 -e 0.0% Test0_base.png Test0.png == -1) {
+#    fail = true
+#}
