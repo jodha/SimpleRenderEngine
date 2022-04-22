2fa8c0909760e46480f77fa0a6a89a037e80aca4
estrac <erikstrack@yahoo.com>
2022-04-22 17:48:28 -0600

    Updated run-test.sh psuedo code to reflect name and location changes

--- test_data/FPS-camera/run-test.sh
+++ test_data/FPS-camera/run-test.sh
@@ -1,7 +1,17 @@
 #! /bin/sh
 
+# Note: this file is currently written in some kind of weird, undocument psuedo-
+#       code that only makes sense to one person on earth (this file primarily
+#       documents the commands that need to be run)
+
 ../../test/SRE-Test-FPS-camera -p test.ui_events
 
-#if (../bin/imgcmp -v -o diff1.png -t 0.0 -e 0.0% Test0_base.png Test0.png == -1) {
-#    fail = true
-#}
+$acc = 0.0 # threshold before a pixel is different (0 fails if any difference)
+$pix = 0.0 # percentage of pixels allowed to be different
+bool pass = true;
+for ($i = 1 to 6) {
+    if(../../../bin/imgcmp -v -o diff$i.png -t $acc -e $pix% gold_files/Test$i.png Test$i.png == -1) {
+        pass = false; 
+    }
+}
+return pass;
